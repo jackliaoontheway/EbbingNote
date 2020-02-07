e50456de0ac83c46a886191ad995bda5043f12ea
Jack <ldhontheway@live.com>
2020-02-07 21:07:16 +0800

    initial ebbing note backend usring spring boot

---
+++ ebbingnote/src/main/java/com/cherryj/ebbingnote/EbbingnoteApplication.java
@@ -0,0 +1,13 @@
+package com.cherryj.ebbingnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbbingnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbbingnoteApplication.class, args);
+	}
+
+}
---
+++ ebbingnote/src/test/java/com/cherryj/ebbingnote/EbbingnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherryj.ebbingnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbbingnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
